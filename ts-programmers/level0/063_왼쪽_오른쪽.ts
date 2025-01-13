0f686eab90c42a514d55e068f367aeecf557e814
devoil <devdevoildev@gmail.com>
2025-01-13 23:23:46 +0900

    ✨ Feat: [프로그래머스][level0] 왼쪽 오른쪽

---
+++ ts-programmers/level0/063_왼쪽_오른쪽.ts
@@ -0,0 +1,17 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181890
+
+function solution(str_list: string[]): string[] {
+  const lIndex = str_list.indexOf("l");
+  const rIndex = str_list.indexOf("r");
+
+  if (lIndex === -1 && rIndex === -1) return [];
+
+  if (lIndex !== -1 && (rIndex === -1 || lIndex < rIndex)) {
+    return str_list.slice(0, lIndex);
+  } else {
+    return str_list.slice(rIndex + 1);
+  }
+}
+
+// test
+console.log(solution(["u", "u", "l", "r"]));
