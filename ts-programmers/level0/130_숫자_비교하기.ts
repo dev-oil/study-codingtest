79da716e54f7396c6908f72c25d8b6c2859d454b
devoil <devdevoildev@gmail.com>
2025-01-16 21:33:49 +0900

    ✨ Feat: [프로그래머스][level0] 숫자 비교하기

---
+++ ts-programmers/level0/130_숫자_비교하기.ts
@@ -0,0 +1,8 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120807
+
+function solution(num1: number, num2: number): number {
+  return num1 === num2 ? 1 : -1;
+}
+
+// test
+console.log(solution(3, 2)); // -1
