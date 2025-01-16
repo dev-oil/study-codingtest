07ec165c6c52f6f7297a8e176ae91c7a9f46eef5
devoil <devdevoildev@gmail.com>
2025-01-17 01:06:00 +0900

    ✨ Feat: [프로그래머스][level1] 문자열을 정수로 바꾸기

---
+++ ts-programmers/level1/001_문자열을_정수로_바꾸기.ts
@@ -0,0 +1,8 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12925
+
+function solution(s: string[]): number {
+  return Number(s);
+}
+
+// test
+console.log(solution("-1234")); // typeof 확인 시 number 나옴
