f3fb276bc9d81c2b51ef60062e3d0012cce6d1ce
devoil <devdevoildev@gmail.com>
2025-01-16 21:33:29 +0900

    ✨ Feat: [프로그래머스][level0] 몫 구하기기

---
+++ ts-programmers/level0/128_몫_구하기.ts
@@ -0,0 +1,8 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120805
+
+function solution(num1: number, num2: number): number {
+  return Math.floor(num1 / num2);
+}
+
+// test
+console.log(solution(7, 2)); // 2
