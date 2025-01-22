1da65540fce958733e7f609fef16dcaceeabf4c4
devoil <devdevoildev@gmail.com>
2025-01-22 16:55:56 +0900

    ✨ Feat: [프로그래머스][level0] 소문자로 바꾸기

---
+++ ts-programmers/level0/077_소문자로_바꾸기.ts
@@ -0,0 +1,8 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181876
+
+function solution(myString: string): string {
+  return myString.toLowerCase();
+}
+
+// test
+console.log(solution("aBcDeFg")); // abcdefg
