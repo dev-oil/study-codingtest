21397e857bb834a9de0d4e840698293b4a33119a
devoil <devdevoildev@gmail.com>
2025-01-22 15:34:09 +0900

    ♻️ Refactor: [프로그래머스][level1] 003

--- ts-programmers/level1/003_자릿수_더하기.ts
+++ ts-programmers/level1/003_자릿수_더하기.ts
@@ -16,7 +16,8 @@ function solution(n: number): number {
 // lodash 한번 적용해보기 . .
 function solution2(n: number): number {
   return _.sum(
-    _.map(String(n), (digit) => Number(digit)) // 각 문자를 숫자로 변환 후 배열 생성
+    _.map(String(n), Number) // _.map(String(n), (digit) => Number(digit))
+    // 생각해보니 Number 이것도 함수임
   );
 }
 
