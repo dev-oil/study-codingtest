49eef94ecbe7952c4c160ad363871f8a74cac011
devoil <devdevoildev@gmail.com>
2025-01-28 17:20:38 +0900

    ♻️ Refactor: [프로그래머스][level0] 071

--- ts-programmers/level0/071_조건에_맞게_수열_변환하기_1.ts
+++ ts-programmers/level0/071_조건에_맞게_수열_변환하기_1.ts
@@ -1,10 +1,12 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/181882
 
-function solution(arr) {
+function solution(arr: number[]): number[] {
   return arr.map((v) => {
-    if (v >= 50 && v % 2 === 0) return v / 2;
-    if (v < 50 && v % 2 === 1) return v * 2;
-    return v;
+    if (v % 2 === 0) {
+      return v >= 50 ? v / 2 : v;
+    } else {
+      return v < 50 ? v * 2 : v;
+    }
   })
 }
 
