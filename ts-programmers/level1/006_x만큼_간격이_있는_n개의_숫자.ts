a95609d499d82853c2f0a545964d281e87053bc4
devoil <devdevoildev@gmail.com>
2025-01-30 00:46:22 +0900

    ♻️ Refactor: [프로그래머스][level1] 006

--- ts-programmers/level1/006_x만큼_간격이_있는_n개의_숫자.ts
+++ ts-programmers/level1/006_x만큼_간격이_있는_n개의_숫자.ts
@@ -1,10 +1,14 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/12954
 
+// 💡 일반적으로 생각할 수 있는 부분을 확인해야함
+// 간격을 x로 하는 길이 n 짜리 배열이라는 점
 function solution(x: number, n: number): number[] {
   const answer: number[] = [];
+  let num: number = 0;
 
   for (let i = 1; i <= n; i++) {
-    answer.push(x * i);
+    num += x;
+    answer.push(num);
   }
 
   return answer;
@@ -12,4 +16,7 @@ function solution(x: number, n: number): number[] {
 
 // test
 console.log(solution(2, 5));
-console.log(solution(-4, 2));
+// console.log(solution(-4, 2));
+//    3423423 
+// 4395734895
+// ----------
