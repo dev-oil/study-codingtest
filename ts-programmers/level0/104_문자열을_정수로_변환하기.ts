763c2aff85fb8a0f9425845eed9fdf7a5e96da98
devoil <devdevoildev@gmail.com>
2025-02-11 17:09:22 +0900

    ✨ Feat: [프로그래머스][level0] 문자열을 정수로 변환하기

---
+++ ts-programmers/level0/104_문자열을_정수로_변환하기.ts
@@ -0,0 +1,8 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181848
+
+function solution(n_str: string): number {
+  return Number(n_str);
+}
+
+// test
+console.log(solution("10"));
