a669d405ea21905bf4a7b3653fbbe0dc3c2699cd
devoil <devdevoildev@gmail.com>
2025-02-11 17:29:35 +0900

    ♻️ Refactor: [프로그래머스][level0] 096

--- ts-programmers/level0/096_배열_비교하기.ts
+++ ts-programmers/level0/096_배열_비교하기.ts
@@ -1,5 +1,18 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/181856
 
+import _ from 'lodash';
+
+// lodash 적용
+function solution2(arr1, arr2) {
+    if (arr1.length !== arr2.length) return arr1.length > arr2.length ? 1 : -1;
+
+    let sumArr1 = _.sum(arr1);
+    let sumArr2 = _.sum(arr2);
+    
+    return sumArr1 === sumArr2 ? 0 : sumArr1 > sumArr2 ? 1 : -1;
+}
+
+// 첫번째 풀이
 function solution(arr1, arr2) {
     if (arr1.length !== arr2.length) return arr1.length > arr2.length ? 1 : -1;
 
@@ -13,7 +26,5 @@ function solution(arr1, arr2) {
 }
 
 // test
-console.log([49, 13], 	[70, 11, 2]); // -1
-
-
-// +) lodash 풀이 추가하기
+console.log(solution([49, 13], [70, 11, 2])); // -1
+console.log(solution2([49, 13], [70, 11, 2])); // -1
