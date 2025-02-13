b2241fc9a8420d0917d4175e257a78355761f3f6
devoil <devdevoildev@gmail.com>
2025-02-13 10:57:39 +0900

    ♻️ Refactor: [프로그래머스][level1] 012

--- ts-programmers/level1/012_정수_제곱근_판별.ts
+++ ts-programmers/level1/012_정수_제곱근_판별.ts
@@ -1,12 +1,13 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/12934
 
 // 💡 숫자 x의 제곱근을 반환해주는 메서드 Math.sqrt()
-// 💡 숫자 x를를 제곱한 값을 반환해주는 메서드 Math.pow()
+// 💡 숫자 x를 제곱한 값을 반환해주는 메서드 Math.pow()
 function solution(n: number) {
-  return Math.sqrt(n) % 1 === 0 // 나머지 값이 없다면 제곱근 ex. sqrt(3) > 1.7320508075688772
-    ? Math.pow(Math.sqrt(n) + 1, 2) 
+  const sqr = Math.sqrt(n);
+  return sqr % 1 === 0 // 나머지 값이 없다면 제곱근 ex. sqrt(3) > 1.7320508075688772
+    ? Math.pow(sqr + 1, 2) 
     : -1;
 }
 
 // test
-console.log(solution(121));
+console.log(solution(121)); // 144
