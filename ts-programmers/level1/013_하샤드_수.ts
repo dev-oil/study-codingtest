6cadac306b70a59ca8c1bada865e5a091eaf174f
dev-oil <devdevoildev@gmail.com>
2025-02-19 09:47:58 +0900

    ♻️ Refactor: [프로그래머스][level1] 013 코드 나눠서 작성하기

--- ts-programmers/level1/013_하샤드_수.ts
+++ ts-programmers/level1/013_하샤드_수.ts
@@ -1,14 +1,26 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/12947
 
+import _ from 'lodash';
+
+function solution2(x: number): boolean {
+  const number = [...String(x)].map(Number);
+  const sum = _.sum(number);
+
+  return x % sum === 0;
+}
+
 function solution(x: number): boolean {
   let sum = 0;
   
   for (const v of [...String(x)]) {
     sum += Number(v);
   }
-  return x % sum === 0 ? true : false;
+  return x % sum === 0 ? true : false; // ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
 }
 
 // test
 console.log(solution(10)); // true
 console.log(solution(11)); // false
+
+console.log(solution2(10)); // true
+console.log(solution2(11)); // false
