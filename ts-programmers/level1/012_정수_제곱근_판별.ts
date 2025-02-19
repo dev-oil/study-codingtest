4fc7ed27694fa0951e12771ba1b824df2f7b7805
dev-oil <devdevoildev@gmail.com>
2025-02-19 09:28:14 +0900

    ♻️ Refactor: [프로그래머스][level1] 012 Math.sqrt / isInteger

--- ts-programmers/level1/012_정수_제곱근_판별.ts
+++ ts-programmers/level1/012_정수_제곱근_판별.ts
@@ -1,5 +1,11 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/12934
 
+// sqrt 중복 retry...
+function solution3(n: number): number {
+  const x = Math.sqrt(n);
+  return Number.isInteger(x) ? (x + 1) ** 2 : -1; // 💡 Number.isInteger 정수 확인하기 함수
+}
+
 // 쉽게 풀어보자 ㅜ_ㅜ
 // 임의의 양의 정수 n에 대해, n이 어떤 양의 정수 x의 제곱인지 아닌지 판단하려 합니다.
 // n이 양의 정수 x의 제곱이라면 x+1의 제곱을 리턴하고, n이 양의 정수 x의 제곱이 아니라면 -1을 리턴하는 함수를 완성하세요.
