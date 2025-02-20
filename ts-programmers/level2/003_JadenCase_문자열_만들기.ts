2d118e484396f741d98d0a0abb7543fc82a8c39d
devoil <devdevoildev@gmail.com>
2025-02-20 09:03:38 +0900

    ♻️ Refactor: [프로그래머스][level2] 003 코드 해석 - 말 그대로의 코드

--- ts-programmers/level2/003_JadenCase_문자열_만들기.ts
+++ ts-programmers/level2/003_JadenCase_문자열_만들기.ts
@@ -1,12 +1,21 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/12951
 
+
 function solution(s: string): string {
-  return s.split(' ').map((v) => {
-    return [...v].map((c, i) => {
-      return i === 0 ? c.toUpperCase() : c.toLowerCase();
-    }).join('');
-  }).join(' ');
+  return s.split(' ').map(toJadenCase).join(' ');
 }
+
+function toJadenCase(v: string): string {
+  // v가 빈 문자열이 아닐것은 보장됨
+  // assert(v !== "")
+
+  // 💡 코드의 해석
+  // 처음부터 순회하면서 대문자되야할놈은 대문자로, 소문자되야할놈은 소문자로 바꿔라. 그럼 그조건은 뭐냐, 0번째는 대문자 아니면 소문자
+  // >> 💡 간결하게 "0번째는 대문자 아니면 소문자"로 작성해주는 것이 좋다 <<
+
+  // return [...v].map((c, i) => i === 0 ? c.toUpperCase() : c.toLowerCase()).join('');
+  return v[0].toUpperCase() + v.slice(1).toLowerCase();
+} 
 
 // test
 console.log(solution("3people unFollowed me")); // "3people Unfollowed Me"
