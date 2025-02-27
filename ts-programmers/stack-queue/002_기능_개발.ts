426e60bfe1c3a4280fde4e59967abde94bfcebfb
devoil <devdevoildev@gmail.com>
2025-02-27 16:50:55 +0900

    ♻️ Refactor: [프로그래머스][stack-queue] count 초기화 및 구조분해할당

--- ts-programmers/stack-queue/002_기능_개발.ts
+++ ts-programmers/stack-queue/002_기능_개발.ts
@@ -1,43 +1,34 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/42586
 
-// 가독성이 너무 안좋은것 같음
-// 정리를 어떤 순서로 해야할까 ?
+import { zip } from 'lodash';
+
 const solution = (progresses: number[], speeds: number[]): number[] => {
   // 1. 개발 완료 일자 배열
-  const endDate: number[] = progresses.map((v, i) =>
-    Math.ceil((100 - v) / speeds[i])
+  const endDate: number[] = zip(progresses, speeds).map(
+    ([v, s]) => Math.ceil((100 - v!) / s!) // 💡 Non-null assertion operator ! (non-null 단언 연산자)
   );
+  console.log(endDate);
 
   // 2. 일자 + 1 (다음일자)
-  let baseDate: number = endDate[0];
+  const [first, ...rest] = endDate;
+  let baseDate = first;
   let count: number = 0;
   const result: number[] = [];
 
   endDate.forEach((v) => {
-    if (v <= baseDate) {
-      count++;
-    } else {
+    if (v > baseDate) {
       result.push(count);
-      count = 1;
+      count = 0;
       baseDate = v;
     }
+    count++;
+
+    // 여기서 만족하는 상태, i 번째까지구간을 result에 넣고 마지막을 만들고있는 구간이 baseDate까지고 길이 count이다.
   });
   result.push(count);
 
   return result;
 };
-
-// const solution = (progresses: number[], speeds: number[]): number[] => {
-//   // progresses : 개발 현재 진척도
-//   // speeds : 개발 속도
-//   return progresses.map((v, i) => {
-//     let count = 0;
-//     for (let j = v; j < 100; j += speeds[i]) {
-//       count++;
-//     }
-//     return count;
-//   });
-// };
 
 // test
 console.log(solution([93, 30, 55], [1, 30, 5])); // [2, 1]
