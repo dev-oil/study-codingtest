0d0ac424d775d51f0c674abebbf0d0e82fa4c531
dev-oil <devdevoildev@gmail.com>
2025-03-06 15:49:04 +0900

    Refactor: [프로그래머스][level0] 함수형

--- ts-programmers/level0/119_특별한_이차원_배열_1.ts
+++ ts-programmers/level0/119_특별한_이차원_배열_1.ts
@@ -2,12 +2,21 @@
 
 // 💡 이차원 배열 만들기
 
+// Array.from 이용하기
 function solution2(n: number): number[][] {
   return Array.from({ length: n }, (_, i) =>
     Array.from({ length: n }, (_, j) => (i === j ? 1 : 0))
   );
 }
 
+// refactor 함수형
+function solution3(n: number): number[][] {
+  return [...Array(n)].map((_, i) =>
+    [...Array(n)].map((_, j) => (i === j ? 1 : 0))
+  );
+}
+
+// 첫번째 풀이
 function solution(n: number): number[][] {
   const answer = [];
 
@@ -23,4 +32,5 @@ function solution(n: number): number[][] {
 }
 
 // test
-console.log(solution(3));
+// console.log(solution(3)); // [ [ 1, 0, 0 ], [ 0, 1, 0 ], [ 0, 0, 1 ] ]
+console.log(solution3(3)); // [ [ 1, 0, 0 ], [ 0, 1, 0 ], [ 0, 0, 1 ] ]
