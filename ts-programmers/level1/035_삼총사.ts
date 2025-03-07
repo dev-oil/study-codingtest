3a3130bf1d6a9f92a8d56e7122149149d2c74232
dev-oil <devdevoildev@gmail.com>
2025-03-07 14:20:57 +0900

    Refactor: [프로그래머스][level1] 035 재귀를 이용한 배열 조합 구하기

--- ts-programmers/level1/035_삼총사.ts
+++ ts-programmers/level1/035_삼총사.ts
@@ -1,10 +1,44 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/131705
 
+import _ from "lodash";
+
+// 💡 배열 조합 함수 만들기
+// 두번째 풀이 - 재귀함수 이용하여 풀이하기
+function combinations(arr: number[], n: number): number[][] {
+  const res: number[][] = [];
+
+  if (n === 1) {
+    return arr.map((v) => [v]);
+  }
+
+  arr.forEach((v, i, origin_arr) => {
+    const rest_arr = origin_arr.slice(i + 1);
+    const comb = combinations(rest_arr, n - 1);
+    const attach_arr = comb.map((comb_v) => [v, ...comb_v]);
+
+    res.push(...attach_arr);
+  });
+
+  return res;
+}
+
+function solution2(number: number[]): number {
+  const sortedArr = combinations(number, 3);
+  let answer = 0;
+
+  sortedArr.forEach((v) => {
+    if (_.sum(v) === 0) answer++;
+  });
+
+  return answer;
+}
+
+// 첫번째 풀이
 function solution(number: number[]): number {
   let count: number = 0;
 
-  for (let i = 0; i < number.length - 2; i++) {
-    for (let j = i + 1; j < number.length - 1; j++) {
+  for (let i = 0; i < number.length; i++) {
+    for (let j = i + 1; j < number.length; j++) {
       for (let k = j + 1; k < number.length; k++) {
         if (number[i] + number[j] + number[k] === 0) {
           count++;
@@ -16,5 +50,7 @@ function solution(number: number[]): number {
 }
 
 // test
-console.log(solution([-2, 3, 0, 2, -5])); // 2
-console.log(solution([-3, -2, -1, 0, 1, 2, 3])); // 5
+// console.log(solution([-2, 3, 0, 2, -5])); // 2
+// console.log(solution([-3, -2, -1, 0, 1, 2, 3])); // 5
+
+console.log(solution2([-2, 3, 0, 2, -5])); // 2
