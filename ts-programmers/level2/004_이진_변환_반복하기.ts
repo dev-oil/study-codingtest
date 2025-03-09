d6fbcfcd3d542cbf5756abb090a88babfcac4039
devoil <devdevoildev@gmail.com>
2025-03-09 17:40:32 +0900

    Refactor: [프로그래머스][level2] 이진 변환 반복하기

--- ts-programmers/level2/004_이진_변환_반복하기.ts
+++ ts-programmers/level2/004_이진_변환_반복하기.ts
@@ -1,21 +1,45 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/70129
 
+// 💡 idiom, 관용구
+// xs.reduce((x, y) => x + y);
+// countZero += str.split('0').length - 1;
+
 function solution(s: string): number[] {
   let str: string = s;
   let count: number = 0;
-  let countZero: number = 0;
-  let answer: number[] = [];
+  let removeZeroCount: number = 0;
 
-  while (Number(str) !== 1) {
+  while (str !== '1') {
     count++;
-    countZero += str.split("0").length - 1;
-    str = str.replaceAll("0", "").length.toString(2);
+
+    const zeroCount = countZero(str);
+    removeZeroCount += zeroCount;
+
+    const withoutZero = removeZero(str);
+    str = toBinary(withoutZero.length);
   }
 
-  answer.push(count, countZero);
+  return [count, removeZeroCount];
+}
 
-  return answer;
+// 0 의 개수를 세는 함수
+function countZero(str: string) {
+  let count = 0;
+  for (const x of str) {
+    if (x === '0') count++;
+  }
+  return count;
+}
+
+// 0 제거 함수
+function removeZero(str: string): string {
+  return str.replaceAll('0', '');
+}
+
+// 2진법 변환 함수
+function toBinary(num: number): string {
+  return num.toString(2);
 }
 
 // test
-console.log(solution("110010101001"));
+console.log(solution('110010101001'));
