ec75d59444b96888cc526d41c27b328c60e9af0c
devoil <devdevoildev@gmail.com>
2025-03-13 11:41:41 +0900

    Refactor: [프로그래머스][level1] 035 재귀가 for문보다 성능이 안좋은 이유

--- ts-programmers/level1/035_삼총사.ts
+++ ts-programmers/level1/035_삼총사.ts
@@ -4,18 +4,16 @@ import _ from 'lodash';
 
 // 💡 배열 조합 함수 만들기
 // 두번째 풀이 - 재귀함수 이용하여 풀이하기
+// 배열a에서 n개를 뽑는방법은
+// 첫번째 것을 포함하는 경우랑 안포함하는 경우 두 개를 합친 것이다
 function combinations(arr: number[], n: number): number[][] {
-  let res: number[][] = [];
-
-  if (n === 1) {
-    return arr.map((v) => [v]);
+  if (n === 0) {
+    return [[]];
   }
 
-  return arr
-    .map((v, i) =>
-      combinations(arr.slice(i + 1), n - 1).map((comb) => [v, ...comb])
-    )
-    .flat();
+  return arr.flatMap((v, i) =>
+    combinations(arr.slice(i + 1), n - 1).map((comb) => [v, ...comb])
+  );
 }
 
 function solution2(number: number[]): number {
@@ -50,3 +48,25 @@ function solution(number: number[]): number {
 // console.log(solution([-3, -2, -1, 0, 1, 2, 3])); // 5
 
 console.log(solution2([-2, 3, 0, 2, -5])); // 2
+
+// 💡 Q. 메모리적으로 더 좋-은 이유?
+// A. 재귀 호출을 할 때마다 함수를 다시 호출하기 때문에 stack에 계속 함수 실행 컨텍스트가 쌓이게 된다.
+//    > 함수 실행 컨텍스트란? 함수가 실행될 때 생성되는 실행 환경들을 말한다. ex. 변수 / 렉시컬 / this / 호이스팅
+
+// 💡 Q. 근데 ㅋㅋ 호출을 할때마다 함수를 호출하는건 다른 함수도 다 그런데요?
+// A. 재귀 호출시에 쌓이는 호출 스택은 트리 형태로 퍼지면서 스택을 차지하게 되기 때문입니다 ㅜ..
+
+// combinations([1, -1, 2, -2, 3, -3], 3)
+//   ├── 1 + combinations([-1, 2, -2, 3, -3], 2)
+//   │     ├── (-1) + combinations([2, -2, 3, -3], 1)
+//   │     │     ├── (2) + combinations([-2, 3, -3], 0) ✅ [[]] 반환
+//   │     │     ├── (-2) + combinations([3, -3], 0) ✅ [[]] 반환
+//   │     │     ├── (3) + combinations([-3], 0) ✅ [[]] 반환
+//   │     │     ├── (-3) + combinations([], 0) ✅ [[]] 반환
+//   │     ├── (2) + combinations([-2, 3, -3], 1)
+//   │     ├── (-2) + combinations([3, -3], 1)
+//   │     ├── (3) + combinations([-3], 1)
+//   ├── (-1) + combinations([2, -2, 3, -3], 2)
+//   ├── (2) + combinations([-2, 3, -3], 2)
+//   ├── (-2) + combinations([3, -3], 2)
+//   ├── (3) + combinations([-3], 2)
