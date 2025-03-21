bf617393ab68107431753c18c1a76f85e7d2fed3
devoil <devdevoildev@gmail.com>
2025-03-21 12:09:40 +0900

    Docs: [프로그래머스][hash] 003 루프 범위 및 탐구한 풀이에 대해서

--- ts-programmers/hash/003_전화번호_목록.ts
+++ ts-programmers/hash/003_전화번호_목록.ts
@@ -2,6 +2,9 @@
 
 // 두번째 풀이 (해시 이용)
 // 최대 276.08ms, 85MB ㅋㅋㅋㅋ????
+// 💡 이 풀이가 더 좋은 이유
+// 직관적으로 생각할 수 있는 풀이이다. (탐구를 안해도 됨)
+// 루프의 범위를 생각할 수 있다. 전화번호의 범위와 / 전화번호 부 의 범위를 생각해보자.
 function solution2(phone_book: string[]): boolean {
   const phoneSet = new Set(phone_book);
 
@@ -20,11 +23,21 @@ function solution2(phone_book: string[]): boolean {
 // 첫번째 풀이
 // 최대 135.94ms, 79MB
 function solution(phone_book: string[]): boolean {
-  phone_book.sort();
+  // phone_book.sort(); // 💡 이것도 한번 들어가는 풀이
 
+  // for (let i = 0; i < phone_book.length - 1; i++) {
+  //   if (phone_book[i + 1].startsWith(phone_book[i])) {
+  //     return false;
+  //   }
+  // }
+
+  // 모든 전화번호에 대해서 모든 다른 전화번호가 접두사인지 확인
+  // 모든 전화번호에 대해서 모든 접두사가 다른 전화번호가인지 확인
   for (let i = 0; i < phone_book.length - 1; i++) {
-    if (phone_book[i + 1].startsWith(phone_book[i])) {
-      return false;
+    for (let j = i; i < phone_book.length - 1; i++) {
+      if (phone_book[i].startsWith(phone_book[j])) {
+        return false;
+      }
     }
   }
 
