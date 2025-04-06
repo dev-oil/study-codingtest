ebc6526e76aa698f00287b91ffe40cd671d3d365
devoil <devdevoildev@gmail.com>
2025-04-06 23:27:54 +0900

    Refactor: [프로그래머스][exhaustive] 소수 찾기 - generator 순열

--- ts-programmers/exhaustive/002_소수_찾기.ts
+++ ts-programmers/exhaustive/002_소수_찾기.ts
@@ -2,6 +2,33 @@
 
 // 💡 이 문제는 순열(permutations)을 사용해야 함
 // 조합 (combinations) 은 level1 035 삼총사 문제 참고
+
+// generator 버전 순열
+function* permutationsGen(numbers: string[], prefix = ''): Generator<number> {
+  // prefix 에 문자열 값을 먼저 전달해야됨
+  if (prefix.length > 0) {
+    yield Number(prefix);
+  }
+
+  for (let i = 0; i < numbers.length; i++) {
+    const next = numbers[i];
+    const remaining = numbers.slice(0, i).concat(numbers.slice(i + 1));
+
+    yield* permutationsGen(remaining, prefix + next);
+  }
+}
+
+function solution2(numbers: string): number {
+  const result = new Set<number>();
+
+  for (const num of permutationsGen([...numbers])) {
+    result.add(num);
+  }
+
+  return Array.from(result).filter(isPrime).length;
+}
+
+// 기본 순열 풀이
 function permutations(prefix: string, numbers: string[], result: Set<number>) {
   if (prefix.length > 0) {
     result.add(Number(prefix));
