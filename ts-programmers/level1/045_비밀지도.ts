a84ef7ee4dc7ca2c7422aa7f1d605361b3fbbdd1
devoil <devdevoildev@gmail.com>
2025-04-13 20:48:41 +0900

    Refactor: [프로그래머스][level1] 비밀지도

--- ts-programmers/level1/045_비밀지도.ts
+++ ts-programmers/level1/045_비밀지도.ts
@@ -2,6 +2,18 @@
 
 {
   // 두번째 풀이
+  // 💡 (v | arr2[i]) 비트연산 - v와 arr2[i]를 이진수로 변환해서 각 비트를 OR 연산한 결과를 구해주는 코드
+  //     => v | arr2[i] = 9 | 30 = 31
+  // 💡 padStart 자릿수 채워주는 메서드!
+  function solution2(n: number, arr1: number[], arr2: number[]): string[] {
+    return arr1.map((v, i) =>
+      (v | arr2[i])
+        .toString(2)
+        .padStart(n, '0')
+        .replace(/1/g, '#')
+        .replace(/0/g, ' ')
+    );
+  }
 
   // 첫번째 풀이
   const toBinary = (arr: number[], n: number): string[] => {
@@ -25,5 +37,6 @@
   }
 
   // test
-  console.log(solution(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28]));
+  // console.log(solution(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28]));
+  console.log(solution2(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28]));
 }
