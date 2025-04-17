c498a9e012b20ccec524caab914e702863d96c51
devoil <devdevoildev@gmail.com>
2025-04-17 15:32:10 +0900

    Refactor: [프로그래머스][level1] 시저암호 - mod 연산 / 커링, 부분적용

--- ts-programmers/level1/037_시저_암호.ts
+++ ts-programmers/level1/037_시저_암호.ts
@@ -1,15 +1,61 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/12926
 
+// 두번째 풀이
+// caesarCipher 함수 - 💡 modulo 연산 이용 해서 리팩토링
+function caesarCipher(text: string, n: number): string {
+  const lowerAlpha: string = 'abcdefghijklmnopqrstuvwxyz';
+  const upperAlpha: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  if (text === ' ') return ' ';
+
+  const alpha: string = text.toLowerCase() === text ? lowerAlpha : upperAlpha;
+
+  const idx: number = alpha.indexOf(text); // 현재 문자 인덱스
+  const shifted: number = (idx + n) % alpha.length; // 밀어낸 후 인덱스, 범위 안으로 순환시킴
+  return alpha[shifted];
+}
+
+// solution 함수 - 커링 / 부분적용 / 일반
+// 커링 버전
+function curriedCaesarCipher(n: number): (text: string) => string {
+  return (text) => caesarCipher(text, n);
+}
+
+function solution3(s: string, n: number): string {
+  const curriedN = curriedCaesarCipher(n);
+  return [...s].map(curriedN).join('');
+}
+
+// 부분 적용 버전
+const partialCaesar = (n: number) => (text: string) => caesarCipher(text, n);
+
+// function partialCaesar(n: number) {
+//   return function(text: string) {
+//     return caesarCipher(text, n);
+//   };
+// }
+
+function solution4(s: string, n: number): string {
+  const partialN = partialCaesar(n);
+  return [...s].map(partialN).join('');
+}
+
+// 일반 버전
+function solution2(s: string, n: number): string {
+  return [...s].map((text) => caesarCipher(text, n)).join(''); // n 값은 고정임. 더 효율적으로 할 수 있는 방법?? -> 커링! 부분적용도 있슴.
+}
+
+// 첫번째 풀이
 // 💡 중복 줄이기
 function solution(s: string, n: number): string {
-  let answer: string = "";
+  let answer: string = '';
 
   // 대문자 65 ~ 90
   // 소문자 97 ~ 122
 
   for (const v of s) {
-    if (v === " ") {
-      answer += " ";
+    if (v === ' ') {
+      answer += ' ';
       continue;
     }
 
@@ -26,6 +72,8 @@ function solution(s: string, n: number): string {
 }
 
 // test
-console.log(solution("AB", 1));
-console.log(solution("z", 1));
-console.log(solution("a B z", 4));
+// console.log(solution('AB', 1));
+// console.log(solution('z', 1));
+// console.log(solution('a B z', 4));
+
+console.log(solution4('a B z', 4));
