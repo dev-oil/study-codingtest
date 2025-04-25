14f07f953135232757bc4bfe070cf966e49a827d
devoil <devdevoildev@gmail.com>
2025-04-25 14:16:09 +0900

    refactor: [프로그래머스][level2] 점프와 순간 이동 - 재귀 풀이로 변경

--- ts-programmers/level2/013_점프와_순간_이동.ts
+++ ts-programmers/level2/013_점프와_순간_이동.ts
@@ -3,19 +3,15 @@
 // 💡 거꾸로 출발했을 때? 를 생각해보기
 // 💡 짝수 홀수 힌트
 // 이걸 그리디라고 합디다..?
+
 function solution(distance: number): number {
-  let batteryUsage: number = 0;
+  if (distance === 0) return 0;
 
-  while (0 < distance) {
-    if (distance % 2 === 0) {
-      distance /= 2;
-    } else {
-      distance -= 1;
-      batteryUsage += 1;
-    }
+  if (distance % 2 === 0) {
+    return solution((distance /= 2));
+  } else {
+    return 1 + solution((distance -= 1));
   }
-
-  return batteryUsage;
 }
 
-console.log(solution(5));
+console.log(solution(5)); // 2
