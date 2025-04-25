a15a4fdec772baf79a8714751f5732300522e99a
devoil <devdevoildev@gmail.com>
2025-04-25 13:44:46 +0900

    refactor: [프로그래머스][level2] 짝지어 제거하기 - 삼항, at()

--- ts-programmers/level2/012_짝지어_제거하기.ts
+++ ts-programmers/level2/012_짝지어_제거하기.ts
@@ -1,13 +1,15 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/12973
 
 function solution(s: string): number {
-  const answer: string[] = []; // stack
+  const answer: string[] = []; // stack - LIFO
 
-  for (const text of [...s]) {
-    if (answer.length > 0) {
-      answer[answer.length - 1] === text ? answer.pop() : answer.push(text);
+  for (const char of s) {
+    const top = answer.at(-1); // 가장 위 문자
+
+    if (top && top === char) {
+      answer.pop();
     } else {
-      answer.push(text);
+      answer.push(char);
     }
   }
 
