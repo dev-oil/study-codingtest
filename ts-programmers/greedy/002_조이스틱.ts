ad283445be2537c569f45ed9e86f6a8466f52550
devoil <devdevoildev@gmail.com>
2025-05-06 14:01:56 +0900

    feat: [프로그래머스][greedy] 조이스틱

--- ts-programmers/greedy/002_조이스틱.ts
+++ ts-programmers/greedy/002_조이스틱.ts
@@ -10,9 +10,29 @@ import _ from 'lodash';
     return Math.min(up, down);
   }
 
+  function countLeftRightMoves(str: string[]): number {
+    let minMove = str.length - 1;
+    for (let i = 0; i < str.length; i++) {
+      let next = i + 1;
+
+      while (next < str.length && str[next] === 'A') {
+        next++;
+      }
+
+      const goAndBack = i + i + (str.length - next);
+      const backAndGo = i + (str.length - next) * 2;
+
+      minMove = Math.min(minMove, goAndBack, backAndGo);
+    }
+
+    return minMove;
+  }
+
   // 좌 / 우 조작(커서 이동) 최소화 (a일 경우에 넘어가야하는?)
   function solution(name: string): number {
-    return _.sum([...name].map(countUpDownMoves));
+    return (
+      _.sum([...name].map(countUpDownMoves)) + countLeftRightMoves([...name])
+    );
   }
 
   // test
