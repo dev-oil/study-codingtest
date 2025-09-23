2f0681a6f74c4040876cdbac03b4c16a1f5c2d16
devoil <devdevoildev@gmail.com>
2025-09-23 18:10:10 +0900

    refactor: [프로그래머스][level1] 덧칠하기 - section 풀이 및 가독성 관점 풀이 추가

--- ts-programmers/level1/052_덧칠하기.ts
+++ ts-programmers/level1/052_덧칠하기.ts
@@ -6,7 +6,8 @@
     colored: boolean
   }
 
-  const solution = (wallLength: number, roller: number, section: number[]) => {
+  // 1. 상태관리 스타일로 풀이
+  const solution = (wallLength: number, roller: number, section: number[]): number => {
     // 1. 벽 만들기
     const wall: Wall[] = Array.from({length: wallLength}, (_, i) => ({
       index: i + 1,
@@ -37,9 +38,41 @@
   }
 
   // test
-  console.log(solution(8, 4, [2, 3, 6])); // 2
-  console.log(solution(5, 4, [1, 3])); // 1
-  console.log(solution(4, 1, [1, 2, 3, 4])); // 4
+  // console.log(solution(8, 4, [2, 3, 6])); // 2
+  // console.log(solution(5, 4, [1, 3])); // 1
+  // console.log(solution(4, 1, [1, 2, 3, 4])); // 4
+
+  // 2. section 배열로만
+  const solution2 = (wallLength: number, roller: number, section: number[]): number => {
+    let count = 0;
+    let paintedEnd = 0;
+
+    for (const s of section) {
+      if (s <= paintedEnd) continue; // 만약 현재 섹션 구역이 칠해진 끝구역 보다 작다면 (아직 칠해진 부분이라면) 스킵하고
+      paintedEnd = s + roller - 1; // 그게 아니라면 새 롤러 써
+      count++;
+    }
+    return count;
+  }
+
+  // test
+  console.log(solution2(8, 4, [2, 3, 6])); // 2
+  console.log(solution2(5, 4, [1, 3])); // 1
+  console.log(solution2(4, 1, [1, 2, 3, 4])); // 4
+
+  // 3. 동작은 같은데 조금 더 직관적으로
+  const solution3 = (wallLength: number, roller: number, section: number[]): number => {
+    let count = 0;
+    let paintedEnd = 0;
+
+    for (const s of section) {
+      if (s > paintedEnd) { // 만약 끝까지 칠해진 부분의 끝보다 현재 구역의 위치가 더 나와있다면 (칠해진 부분 바깥이라면) 새 롤러 써
+        count++;
+        paintedEnd = s + roller - 1;
+      }
+    }
+    return count;
+  }
 }
 
 // devoil's note
@@ -55,4 +88,7 @@
 // 2-2. 그냥 기존 wall을 이용해서 상태관리 방식으로 false 값 첫번째 인덱스 + m(롤러값) 까지 해서 전부 true로 만들고 계속 반복 및 rolling
 // 일단 상태관리 방식으로 진행했고, 처음엔 시간 초과가 떴음 100,000 제한 ... ^_^
 
+// 두번째 풀이 
+// section으로만 풀어보려고 했음
+// section을 돌면서, 새로 paint 끝 갱신 및 카운트하는 방식으로. 만약 다음 반복턴에 s 가 갱신된 paint 끝보다 작으면 카운트 스킵이죵?
 
